0ecabc8718d3dd46b964d9572fb560151aacdcf6
JemalMoha <jemaltahir83@gmail.com>
2017-05-07 02:48:15 +0300

    version 4 added

--- js/version4.js
+++ js/version4.js
@@ -24,5 +24,10 @@ var todoList = {
     deleteTodo: function(position) {
         this.todos.splice(position,1);
         this.displayTodos();
-    }
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    } 
 };
