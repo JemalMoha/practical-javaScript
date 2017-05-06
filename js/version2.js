63774695cd32fe5d11b0d3e59d278e8e142f6b5f
JemalMoha <jemaltahir83@gmail.com>
2017-05-06 23:35:51 +0300

    version 2 added

---
+++ js/version2.js
@@ -0,0 +1,27 @@
+//V1 Requrements
+//1. It Should have a place to store todos
+//2. It Should have a function to display todos
+//3. It Should have a function to add new toods
+//4. It Should have a function to change a todo
+//5. It Should have a function to delete a todo
+
+var todos = ['item 1', 'item 2', 'item 3']; // to store todos
+// It Should have a function to display todos
+function displayTodos() {
+    console.log('My todos:', todos);
+}
+// It Should have a function to add new toods
+function addTodo(todo) {
+    todos.push('new todo'); 
+    displayTodos();
+}
+// It Should have a function to change a todo
+function changeTodo(position, newValue) {
+    todos[position] = newValue;
+    displayTodos();
+}
+ // It Should have a function to delete a todo
+function deleteTodo(position) {
+    todos.splice(0,1); 
+}
+
